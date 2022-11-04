785ed4ca39241871cb6a59cc45139e15922d691e
DavidDGamble <daviddgamble@gmail.com>
2022-11-04 10:22:34 -0700

    Add Order.prototype.calulate() to README and js. Tested and passed.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Describe: Order()
 
 Test: Should create an object that contains all of the customers order details.
-Code: new Order(large, true, true, false, false, false)
+Code: new Order('large', true, true, false, false, false)
 Expected Outcome: order {
   size: large
   pep: true
@@ -10,3 +10,15 @@ Expected Outcome: order {
   blkOlv: false
   xtraChz: false
 }
+
+Describe: Order.prototype.calulate()
+
+Test: Should calculate the total price for the order in the order object.
+Code: test = new Order('lg', true, true, false, false, false)
+test.calculate() 
+Expected Output: 14
+
+Test: Should calculate the total price for the order in the order object when a price value === 0.5.
+Code: test = new Order('lg', true, true, true, false, false)
+test.calculate() 
+Expected Output: 14.5
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
 // Business Logic -------------------------------------------------------------
+const prices = {
+  sm: 8,
+  md: 10,
+  lg: 12,
+  xlg: 15,
+  pep: 1,
+  saus: 1,
+  mush: 0.5,
+  blkOlv: 0.5,
+  xtraChz: 1
+}
+
 function Order(size, pep, saus, mush, blkOlv, xtraChz) {
   this.size = size;
   this.pep = pep;
@@ -7,5 +19,19 @@ function Order(size, pep, saus, mush, blkOlv, xtraChz) {
   this.blkOlv = blkOlv;
   this.xtraChz = xtraChz
 }
+
+Order.prototype.calculate = function() {
+  let priceTotal = 0;
+  for (let key in this) {
+    if (key === 'size') {
+      priceTotal += prices[this[key]];
+    } else {
+      if (this[key] === true) {
+        priceTotal += prices[key];
+      }
+    }
+  }
+  return priceTotal;
+};
 
 // UI Logic -------------------------------------------------------------
