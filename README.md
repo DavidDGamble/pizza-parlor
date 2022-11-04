5aecbd2d6875c4f8dfeb4a7546f8cb4695308baa
DavidDGamble <daviddgamble@gmail.com>
2022-11-04 09:26:13 -0700

    Add Order() constructor function to README and js. Tested and passed.

---
+++ README.md
@@ -0,0 +1,12 @@
+Describe: Order()
+
+Test: Should create an object that contains all of the customers order details.
+Code: new Order(large, true, true, false, false, false)
+Expected Outcome: order {
+  size: large
+  pep: true
+  saus: true
+  mush: false
+  blkOlv: false
+  xtraChz: false
+}
---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// Business Logic -------------------------------------------------------------
+function Order(size, pep, saus, mush, blkOlv, xtraChz) {
+  this.size = size;
+  this.pep = pep;
+  this.saus = saus;
+  this.mush = mush;
+  this.blkOlv = blkOlv;
+  this.xtraChz = xtraChz
+}
+
+// UI Logic -------------------------------------------------------------
